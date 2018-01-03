c009892ae38d5e54f24c115a823c81c42f5b8154
Dalliya <dalliyal@gmail.com>
2018-01-03 23:39:08 +0200

    init commit

---
+++ Task_12_a.py
@@ -0,0 +1,10 @@
+number = input()
+
+def sum_of_digits(number):
+    result = int(number[0]) + int(number[1]) + int(number[2])
+    return result
+
+result = sum_of_digits(number)
+print(result)
+
+
