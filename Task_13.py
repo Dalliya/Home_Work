9a463dcce160ae2e99e55385475d390a076bb50f
Dalliya <dalliyal@gmail.com>
2018-01-04 20:45:16 +0200

    init commit

--- Task_13.py
+++ Task_13.py
@@ -1,10 +1,10 @@
 import math
 
-a = int(input())
-b = int(input())
+catheter1 = a = int(input("Введите катет 1 прямоугольного треугольника:"))
+catheter2 = b = int(input("Введите катет 2 прямоугольного треугольника:"))
 
 def triangle_square_and_perimeter(a, b):
-    result = float(a * b / 2), float(a + b + math.sqrt(a**2 + b**2))
+    result = (a * b / 2), (a + b + math.sqrt(a**2 + b**2))
     return result
 
 result = triangle_square_and_perimeter(a, b)
