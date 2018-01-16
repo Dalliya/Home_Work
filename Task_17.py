ec8f9f2998141fda7a1a2c44491e71ddb4374196
Dalliya <dalliyal@gmail.com>
2018-01-16 22:50:18 +0200

    init commit

--- Task_17.py
+++ Task_17.py
@@ -11,13 +11,13 @@ b = float(input("Введите значение b = "))
 c = float(input("Введите значение c = "))
 
 def solve_quadratic_equation(a, b, c):
-    D = b**2 - 4*a*c
-    if D > 0 and a != 0:
-        result_1 = (-b + math.sqrt(D)) / (2 * a)
-        result_2 = (-b - math.sqrt(D)) / (2 * a)
+    d = b**2 - 4*a*c
+    if d > 0 and a != 0:
+        result_1 = (-b + math.sqrt(d)) / (2 * a)
+        result_2 = (-b - math.sqrt(d)) / (2 * a)
         return result_1, result_2
 
-    if D == 0:
+    if d == 0:
         result = -b / (2 * a)
         return result, None
 
