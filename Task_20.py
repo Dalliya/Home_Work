4acd947cf13a26854175adf9783aeb71b936d86a
Dalliya <dalliyal@gmail.com>
2018-01-20 21:41:37 +0200

    init commit

---
+++ Task_20.py
@@ -0,0 +1,27 @@
+#Написать функцию для поиска разницы сумм всех четных и всех нечетных чисел среди 100 случайно сгенерированных чисел в произвольном числовом диапазоне.
+#Т.е. от суммы четных чисел вычесть сумму нечетных чисел.
+#		def diff_even_odd(num_limit, lower_bound, upper_bound): # returns int
+#		     pass
+
+import random
+
+lower_bound = int(input())
+upper_bound = int(input())
+num_limit = 101
+
+def diff_even_odd(num_limit, lower_bound, upper_bound):
+    rand_even_total = 0
+    rand_odd_total = 0
+    for i in range(num_limit):
+        rand_num = random.randint(lower_bound, upper_bound)
+        print(rand_num)
+        if rand_num % 2 == 0:
+            rand_even_total += rand_num
+        elif rand_num % 2 != 0:
+            rand_odd_total += rand_num
+
+    diff = rand_even_total - rand_odd_total
+    return diff
+
+print(diff_even_odd(num_limit, lower_bound, upper_bound))
+
