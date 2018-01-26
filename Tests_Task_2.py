e1d2a1b43350619e3b2000349b3bb7fc4fc60dcb
Dalliya <dalliyal@gmail.com>
2018-01-26 20:33:22 +0200

    init commit

---
+++ Tests_Task_2.py
@@ -0,0 +1,16 @@
+a = float(input('Введите число a: '))
+b = float(input('Введите число b: '))
+c = float(input('Введите число c: '))
+
+result = a - 4 * b / c
+
+print("Результат выражения (a - 4 * b / c) при a = %d , b = %d и c = %d равен: %.3f" % (a, b, c, result))
+
+
+
+
+
+
+
+
+
