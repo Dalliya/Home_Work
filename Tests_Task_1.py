94426ecb6cd9e6d34b05c3b33c53e0e0699b94d2
Dalliya <dalliyal@gmail.com>
2018-01-26 20:26:30 +0200

    init commit

---
+++ Tests_Task_1.py
@@ -0,0 +1,7 @@
+a = float(input('Введите число a: '))
+b = float(input('Введите число b: '))
+c = float(input('Введите число c: '))
+
+result = (a + b * c)**2
+
+print("Результат выражения (a + b * c)**2 при a = %d , b = %d и c = %d равен: %.3f" % (a, b, c, result))
